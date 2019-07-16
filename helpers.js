263b9ec37a5fcf8785d5f9c34b8e394719c9447d
mahume <michael.a.hume@gmail.com>
2019-07-16 16:18:39 -0600

    Adding icons

--- helpers.js
+++ helpers.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+exports.icon = name => fs.readFileSync(`./public/images/icons/${name}.svg`);
+
+exports.siteName = 'Xplora';
+
 exports.menu = [
   { slug: '/stores', title: 'Stores', icon: 'store' },
   { slug: '/tags', title: 'Tags', icon: 'tag' },
