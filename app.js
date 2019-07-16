a8bd83569a2ec8678d6f0871afb1c9dc9e193a93
mahume <michael.a.hume@gmail.com>
2019-07-16 17:25:55 -0600

    Adding error handlers

--- app.js
+++ app.js
@@ -9,31 +9,32 @@ const passport = require('passport');
 const promisify = require('es6-promisify');
 const flash = require('connect-flash');
 const expressValidator = require('express-validator');
+const routes = require('./routes/index');
 const helpers = require('./helpers');
-const routes = require('./routes');
+const errorHandlers = require('./handlers/errorHandlers');
 
-// Initializing Express
+// Start Express app
 const app = express();
 
-// Pug View Engine
+// Pug view engine
 app.set('views', path.join(__dirname, 'views')); // Pug files folder
 app.set('view engine', 'pug');
 
-// Static Files
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Parse requests on req.body
+// Takes requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Data validation
+// Methods for validating data. userController.validateRegister
 app.use(expressValidator());
 
-// req.cookies
+// Populates req.cookies
 app.use(cookieParser());
 
-// Store data on visitors from request to request
-// This keeps users logged in and allows us to send flash messages
+// Store session data on visitors from request to request
+// Keeps users logged in and allows us to send flash messages
 app.use(
   session({
     secret: process.env.SECRET,
@@ -44,14 +45,14 @@ app.use(
   })
 );
 
-// Logins with Passport
+// Logins
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Passes error to next page request
+// Pass eror message to the next page the user requests
 app.use(flash());
 
-// Pass Pug Variables
+// Pass variables to our templates + all requests
 app.use((req, res, next) => {
   res.locals.h = helpers;
   res.locals.flashes = req.flash();
@@ -66,7 +67,20 @@ app.use((req, res, next) => {
   next();
 });
 
-// Handle routes
 app.use('/', routes);
+
+// 404 and forward to error handler
+app.use(errorHandlers.notFound);
+
+// Check if errors are just validation errors
+app.use(errorHandlers.flashValidationErrors);
+
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// Production error handler
+app.use(errorHandlers.productionErrors);
 
 module.exports = app;
