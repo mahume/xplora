24d5e142ac4c115a34be9ad23e7b3ac6a6c893e3
mahume <michael.a.hume@gmail.com>
2019-07-15 21:42:04 -0600

    Adding devdeps, and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,25 @@
+const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
+
+const javascript = {
+  test: /\.(js)$/,
+  use: [
+    {
+      loader: 'babel-loader',
+      options: { presets: ['env'] },
+    },
+  ],
+};
+
+const postcss = {
+  loader: 'postcss-loader',
+  options: {
+    plugins() {
+      return [autoprefixer({ browsers: 'last 3 versions ' })];
+    },
+  },
+};
 
 const config = {
   entry: {
