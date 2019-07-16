5835d20c7fbe620fc9125cb61f86c3148c97eabe
mahume <michael.a.hume@gmail.com>
2019-07-16 16:14:35 -0600

    Adding tabs data

---
+++ helpers.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+exports.menu = [
+  { slug: '/stores', title: 'Stores', icon: 'store' },
+  { slug: '/tags', title: 'Tags', icon: 'tag' },
+  { slug: '/top', title: 'Top', icon: 'top' },
+  { slug: '/add', title: 'Add', icon: 'add' },
+  { slug: '/map', title: 'Map', icon: 'map' },
+];
